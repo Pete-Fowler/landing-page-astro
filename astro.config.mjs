07781508a23a348aeea8c24fff4b5fe89168c1bd
Pete Fowler <fowler.pete@gmail.com>
2024-08-27 19:15:00 -0600

    Add react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://ram-ghale.netlify.app",
-  integrations: [mdx()]
+    integrations: [mdx(), react()],
 });
