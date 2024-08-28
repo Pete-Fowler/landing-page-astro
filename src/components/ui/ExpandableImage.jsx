77f8ec5ae80fbf8852ddd2f8a2587e9b518b6d8b
Pete Fowler <fowler.pete@gmail.com>
2024-08-27 19:16:02 -0600

    Add image modal component

---
+++ src/components/ui/ExpandableImage.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import "./ImageModal.css";
+
+export default function ImageModal({ images }) {
+    const [isOpen, setIsOpen] = useState(false);
+    const [currentImage, setCurrentImage] = useState(null);
+
+    const openModal = (image) => {
+        setCurrentImage(image);
+        setIsOpen(true);
+    };
+
+    const closeModal = () => {
+        setIsOpen(false);
+        setCurrentImage(null);
+    };
+    console.log("Render");
+    return (
+        <>
+            <div className="thumbnails">
+                {images.map((image, index) => (
+                    <img
+                        key={index}
+                        src={image.src}
+                        alt={image.alt}
+                        className="thumbnail"
+                        onClick={() => openModal(image)}
+                    />
+                ))}
+            </div>
+
+            {isOpen && (
+                <div className="modal" onClick={closeModal}>
+                    <span className="close" onClick={closeModal}>
+                        &times;
+                    </span>
+                    <img
+                        src={currentImage.src}
+                        alt={currentImage.alt}
+                        className="modal-image"
+                    />
+                </div>
+            )}
+        </>
+    );
+}
